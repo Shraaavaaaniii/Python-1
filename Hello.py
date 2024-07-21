e6df4d51f78a92906352b8e531385d1cdecc44d8
Shraaavaaaniii <140896891+Shraaavaaaniii@users.noreply.github.com>
2024-07-21 21:19:15 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-#python program to add two numbers
+#python program to add two numbers with a and b
 a = int(input("Enter first number : "))
 b = int(input("Enter second number : "))
 print(a,"+",b,"=",a+b)
