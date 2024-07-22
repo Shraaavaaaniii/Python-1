5d1a68f4b0b39a873c9cc785ed79e07a074c4abc
Shraaavaaaniii <140896891+Shraaavaaaniii@users.noreply.github.com>
2024-07-22 15:33:50 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,46 @@
+#Data types in python
+#int 
+a = 100
+print(a,type(a))
+#float
+a = 3.14
+print(a,type(a))
+#complex
+a = 1+2j
+print(a,type(a))
+#string
+a = "Shravani"
+print(a,type(a))
+#list
+a = ["Pune","Kolhapur","Mumbai"]
+print(a,type(a))
+#tuple
+a = ("Rose","Hibiscus","Lotus")
+print(a,type(a))
+#dict
+a = {"name" : "Shravani","Roll No." : 5,"age" : 20}
+print(a,type(a))
+#None type
+a = None 
+print(a,type(a))
+#bool
+a = False
+print(a,type(a))
+#set
+a = {1,2,3,4,5,6}
+print(a,type(a))
+#frozenset
+a = ({"Mango","Apple","Banana"})
+print(a,type(a))
+#range
+a = range(5)
+print(a,type(a))
+#bytes
+a = b"Hello"
+print(a,type(a))
+#bytearray
+a = bytearray(5)
+print(a,type(a))
+#memoryview
+a = memoryview(bytes(5))
+print(a,type(a))
