c9237565d928a0873e6d2507cd08d2e13b822c43
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-07-23 06:37:14 +0530

    "I made some change"

--- operators.py
+++ operators.py
@@ -1,60 +1,6 @@
 #Operators in python
 a = 5
 b = 2
-#Arithmetic operators
-print("Arithmetic operators")
-print(a,"+",b,"=",a+b)
-print(a,"-",b,"=",a-b)
-print(a,"*",b,"=",a*b)
-print(a,"/",b,"=",a/b)
-print(a,"%",b,"=",a%b)
-print(a,"//",b,"=",a//b)
-#Assignment operators
-print("Assignment operators")
-x = 5
-x += 5
-print("x += 5",x)
-x -= 5
-print("x -= 5",x)
-x *= 5
-print("x *= 5",x)
-x /= 5
-print("x /= 5",x)
-x **= 5
-print("x **= 5",x)
-x %= 5
-print("x %= 5",x)
-x //= 5
-print("x //= 5",x)
-#Comparison operators
-print("Comparison operators")
-x = 10
-y = 30
-print(x==y)
-print(x!=y)
-print(x>y)
-print(x<y)
-print(x>=y)
-print(x<=y)
-#logical operators
-a = True
-b = False
-print("Logical operators")
-print(a and b)
-print(a or b)
-print(not(a))
-#bitwise operators
-a = 4
-b = 5
-print(a,"&",b,"=",a&b)
-print(a,"|",b,"=",a|b)
-print(a,"^",b,"=",a^b)
-print("~",a,"=",~a)
-print(a,"<<1=",a<<1)
-print(b,">>1=",b>>1)
-#Operators in python
-a = 5
-b = 2
 #1.Arithmetic operators
 print("Arithmetic operators")
 print(a,"+",b,"=",a+b)
