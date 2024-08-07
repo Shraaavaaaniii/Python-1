2e30d3986cc9cf0b16d2bec55f0a7b2ac996e01e
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-08-07 18:30:51 +0530

    Initial comment

---
+++ for_.py
@@ -0,0 +1,10 @@
+# 1.python program to all natural number upto n
+# n = int(input("Enter any natural number : "))
+# for i in range(1,n+1):
+#     print(i,end=" ")
+
+# 2. python program to print even numbers upto n 
+n = int(input("Enter any natural number :"))
+for i in range(1,30):
+    if n%2 == 0:
+        print(i,end=" ")
