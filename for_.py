610c34d6a7afbab3f3d8f4c167c88e43526b55ed
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-08-07 18:40:40 +0530

    Added more programs

--- for_.py
+++ for_.py
@@ -4,7 +4,15 @@
 #     print(i,end=" ")
 
 # 2. python program to print even numbers upto n 
-n = int(input("Enter any natural number :"))
-for i in range(1,30):
-    if n%2 == 0:
-        print(i,end=" ")
+# n = int(input("Enter any natural number :"))
+# for i in range(1,n+1):
+#     if i%2 == 0:
+#         print(i,end=" ")
+
+# 3. Python program to find odd numbers up to n
+# n = int(input("Enter any natural number : "))
+# for i in range(1,n+1):
+#     if i%2 != 0:
+#         print(i,end=" ")
+
+# 4. Python program to print 1 2 4 8 16 32...
