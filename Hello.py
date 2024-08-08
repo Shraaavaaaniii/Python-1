7beb2114bae2630498ae14246f07cc692eda6cb6
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-08-08 15:14:30 +0530

    delete some code

--- Hello.py
+++ Hello.py
@@ -3,7 +3,3 @@ a = int(input("Enter first number : "))
 b = int(input("Enter second number : "))
 print(a,"+",b,"=",a+b)
 
-#python program to add two numbers
-a = int(input("Enter first number : "))
-b = int(input("Enter second number : "))
-print(a,"+",b,"=",a+b)
