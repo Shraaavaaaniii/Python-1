e65ff9802186fa2aa11566c84ee73cfa2338f94a
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-08-12 15:25:40 +0530

    initial commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,7 @@
-# l.pop() #It will remove the last element
-# print(l)
-# l.pop(0) #It will remove the element at index 0 from list
-# print(l)
-
-# 4.remove():Removes specified element from the list
-# l = ['a','b','c','d']
-# l.remove('c')
-# print(l)
-
-# 5.index(): Returns the index of first occurence of element from the list
-# l = ["Mango","Orange","Pineapple","Guava"]
-# print(l.index("Pineapple"))
-
-# 6.count() : Returns the count of the element present in the list
+t = (1,2,3,4,5,6)
+# print(t,type(t))
+#we can access tuple elements by using +ve or -ve indexing
+# print(t[2])
+# by using for loop
+# for i in t:
+#     print(i)
---
+++ tuple_.py
@@ -0,0 +1,21 @@
+t = (1,2,3,4,5,4,6)
+print(t,type(t),len(t))
+#we can access tuple elements by using +ve or -ve indexing
+# print(t[2])
+# by using for loop
+# for i in t:
+#     print(i)
+
+# index(element),index(element,start),index(element,start,end)
+# print("The element 4 comes at position",t.index(4))
+# print("The occurence of 4 after index 3 is",t.index(4,4))
+# print("The occurence of 4 after index 3 and before index 6 is",t.index(4,4,6))
+
+# count()
+# print("5 comes in tuple for",t.count(5),"times")
+
+# min()
+# print("Minimum element present in the list is",min(t))
+
+# max()
+# print("Maximum element present in list is",max(t))
