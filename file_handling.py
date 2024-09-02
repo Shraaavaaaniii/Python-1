2a837db389a131c0617df0ab8610d48d9c9c6e76
Shraaavaaaniii <patilshravani302004@gmail.com>
2024-09-02 15:48:51 +0530

    initial commit

--- file_handling.py
+++ file_handling.py
@@ -1,11 +1,11 @@
 #File handling
 #to create a new file
 # f.write
-# f = open("demo",'w')
-# f.write("Twinkle Twinkle little star, how I wonder what you are")
-f = open("demo",'r')
-print(f.read())
-print(f.tell())
-f.seek(20)
-print(f.tell())
-f.close()
+f = open("demo.txt",'w')
+f.write("Hello my name is Asman")
+# f = open("demo",'r')
+# print(f.read())
+# print(f.tell())
+# f.seek(20)
+# print(f.tell())
+# f.close()
